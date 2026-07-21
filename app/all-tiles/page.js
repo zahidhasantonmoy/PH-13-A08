a261ae11b43638c330feba0d12f78c0a6a59ea83
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2026-07-21 22:50:06 +0600

    fixing all tiles page

--- app/all-tiles/page.js
+++ app/all-tiles/page.js
@@ -1,7 +1,5 @@
 import AllTilesClient from "@/components/AllTilesClient";
 
-import AllTilesClient from "@/components/AllTilesClient";
-
 export default async function AllTilesPage() {
   const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/api/tiles");
   const allTiles = await res.json();
