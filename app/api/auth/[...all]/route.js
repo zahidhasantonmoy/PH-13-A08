3e845e0f0c3c9c614c2f1426ecd3808dd73ffd59
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2026-07-22 09:10:30 +0600

    trying to onrender

--- app/api/auth/[...all]/route.js
+++ app/api/auth/[...all]/route.js
@@ -1,16 +1,4 @@
-import { getAuth } from "@/lib/auth";
+import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export const dynamic = "force-dynamic";
-
-export async function GET(request) {
-  const auth = await getAuth();
-  const handler = toNextJsHandler(auth);
-  return handler.GET(request);
-}
-
-export async function POST(request) {
-  const auth = await getAuth();
-  const handler = toNextJsHandler(auth);
-  return handler.POST(request);
-}
+export const { GET, POST } = toNextJsHandler(auth);
