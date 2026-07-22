2849366487623e7155085bc985314bf9d7a7cf4a
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2026-07-22 08:54:54 +0600

    mongodb fetching problem solved

--- app/api/auth/[...all]/route.js
+++ app/api/auth/[...all]/route.js
@@ -1,4 +1,6 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
+export const dynamic = "force-dynamic";
+
 export const { GET, POST } = toNextJsHandler(auth);
